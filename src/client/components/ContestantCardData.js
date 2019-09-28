2a4ae75a4cca775e8d1159fe6a31292d553e2169
Anthony Nguyen <anguyee@gmu.edu>
2019-09-27 23:48:07 -0700

    Updated code to render modal on vote; WIP: Update Modal Copy and final formatting

--- src/client/components/ContestantCardData.js
+++ src/client/components/ContestantCardData.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
 
-const onClicker = (cheese) => {
 
 
-  <Modal open={cheese}> </Modal>
-}
 
-const ContestantCardData = ({ contestant }) => (
-
-  <div key={contestant.id} className="contestant-information">
-    <div className="contestant-content">
-      <img src={contestant.image} alt={contestant.name} />
-      <h2> {contestant.name}</h2>
-      <p>{contestant.bio}</p>
-      <a href>Link to Portfolio</a>
-      <Button variant="contained" color="primary" onClick={onClicker(true)}>
-        
-      Please Vote Yo
-    </Button>
+class ContestantCardData extends PureComponent {
+    openModal = () => {
+      const {handleOpenModal} = this.props;
+      handleOpenModal();
+    };
+    
+    render = () => (
+      <div key={this.props.contestant.id} className="contestant-information">
+      <div className="contestant-content">
+        <img src={this.props.contestant.image} alt={this.props.contestant.name} />
+        <h2> {this.props.contestant.name}</h2>
+        <p>{this.props.contestant.bio}</p>
+        <a href = {this.props.contestant.link}>Link to Portfolio</a>
+        <Button variant="contained" color="primary" onClick={this.openModal}>
+        Please Vote Yo
+      </Button>
+      </div>
     </div>
-  </div>
-);
+    )
+    }
+
 
 export default ContestantCardData;
--- src/client/components/ContestantRender.js
+++ src/client/components/ContestantRender.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import FacebookRender from "./FacebookRender";
 import ContestantCardData from "./ContestantCardData";
-import Modal from '@material-ui/core/Modal';
-//const API = "http://stateapi-test.votenow.tv/widgets/get?wid=";
-const API = "http://localhost:8080/api";
+import Button from '@material-ui/core/Button';
+import Modal from "@material-ui/core/Modal";
 
-const QUERY = "31204006f9270601";
+
+const API = "http://localhost:8080/api";
 
 class ContestantRender extends Component {
   state = {
@@ -13,12 +13,23 @@ class ContestantRender extends Component {
     loggedOut: {},
     loggedIn: {},
     error: "",
-    isOpen: true
+    open:false
     };
 
   loginHandler = isLoggedIn => {
     this.setState({ isLoggedIn });
   };
+
+  handleOpen = () => {
+    // get which button was pressed via `stationNumber`
+    // open the modal and set the `stationNumber` state to that argument
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
 
   componentDidMount() {
     fetch(API)
@@ -51,9 +62,24 @@ class ContestantRender extends Component {
                 <ContestantCardData
                   key={contestant.id}
                   contestant={contestant}
+                  handleOpenModal={this.handleOpen}
                 />
               ))}
             </div>
+            <Modal 
+            open={this.state.open} 
+            onClose={this.handleClose}  
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description">
+            {/* display the content based on newly set state */}
+            <div className = "modal">
+            <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+            </div>
+          </Modal>
+
           </div>
       </>
     );
